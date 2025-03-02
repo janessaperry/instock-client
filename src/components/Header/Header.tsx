644c0057f550f8dbfe8417619f6a44799f6a725f
janessaperry <hello@janessaperry.com>
2025-03-02 11:18:09 -0800

    add and style header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,29 @@
+import { NavLink, Link } from "react-router-dom";
+import InStockLogo from "../../assets/logo/InStock-logo.svg";
 import "./Header.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__logo" src={InStockLogo} alt="InStock Logo" />
+      </Link>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <NavLink to="/" className="header__nav-link">
+              Warehouses
+            </NavLink>
+          </li>
+          <li className="header__nav-item">
+            <NavLink to="/inventory" className="header__nav-link">
+              Inventory
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
